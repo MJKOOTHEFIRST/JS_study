be9a3504548fb6b89a7190b665e7cdc5697a243d
su <su@nstek.com>
2023-11-28 19:05:45 +0900

    주석 추가

--- chart_server.js
+++ chart_server.js
@@ -9,7 +9,9 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-app.get('/e_day', (req, res) => {
+
+app.get('/e_day', (req, res) => { //실제 시스템의 경로나 폴더를 나타내는게 아니라 엔드포인트! url의 일부로 임의로 정함
+    // 이 경로는 서버의 api 설계에 따라 생성되며, 실제 파일 시스템의 구조와 관련이 없다. 
     fs.readFile('./e_day.conf', 'utf8', (err, data) => {
         if (err) {
             res.status(500).send('파일을 읽을 수 없습니다.');
